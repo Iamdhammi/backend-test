c57f271c53711f87373cb01cdc473c56b9d52b53
Bello Damilola <user@users-MacBook-Pro.local>
2020-06-25 00:39:57 +0100

    Heroku error log added

--- config/app.php
+++ config/app.php
@@ -123,6 +123,9 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
+    // heroku error log
+    "log"=>"errorlog",
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
